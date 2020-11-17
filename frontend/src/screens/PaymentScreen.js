ffa0b9deac90e9354202ddf399dffbe698788eac
HX17 <u4983817@gmail.com>
2020-11-17 19:10:23 +1100

    complete PlaceOrder screen

---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Form, Button, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
+import { savePaymentMethod } from "../actions/cartActions";
+
+const PaymentScreen = ({ history }) => {
+  const [paymentMethod, setPaymentMethod] = useState("Paypal");
+
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  if (!shippingAddress) {
+    history.push("/shipping");
+  }
+
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    history.push("/placeorder");
+  };
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group>
+          <Form.Label as="legend">Select Method</Form.Label>
+
+          <Col>
+            <Form.Check
+              type="radio"
+              label="PayPal or Credit Card"
+              id="Paypal"
+              name="paymentMethod"
+              value="Paypal"
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+          </Col>
+
+          {/* <Col>
+            <Form.Check
+              type="radio"
+              label="Strip"
+              id="Strip"
+              name="paymentMethod"
+              value="Strip"
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+          </Col> */}
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default PaymentScreen;
